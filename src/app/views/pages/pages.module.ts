97f23fdbabd3d89bc860ca96310871e44cc5ae18
Kamil Adylov <kradylov@gmail.com>
2019-11-07 16:12:57 -0700

    added reviewer component

--- src/app/views/pages/pages.module.ts
+++ src/app/views/pages/pages.module.ts
@@ -11,9 +11,10 @@ import { MailModule } from './apps/mail/mail.module';
 import { ECommerceModule } from './apps/e-commerce/e-commerce.module';
 import { UserManagementModule } from './user-management/user-management.module';
 import { MyPageComponent } from './my-page/my-page.component';
+import { ReviewerComponent } from './reviewer/reviewer.component';
 
 @NgModule({
-	declarations: [MyPageComponent],
+	declarations: [MyPageComponent, ReviewerComponent],
 	exports: [],
 	imports: [
 		CommonModule,
