3f95d7d8d263f60a90347bc60efe72aa539208c5
Scott Nedvesky <snedvesky@Scotts-MacBook-Pro-3.local>
2020-02-20 17:06:07 -0700

    Updated Prereview component, logo of the project

--- src/app/views/pages/admin/incoming-work/incoming-work.component.ts
+++ src/app/views/pages/admin/incoming-work/incoming-work.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'kt-incoming-work',
   templateUrl: './incoming-work.component.html',
   styleUrls: ['./incoming-work.component.scss']
 })
+
+
 export class IncomingWorkComponent implements OnInit {
 
   constructor() { }
@@ -13,3 +16,4 @@ export class IncomingWorkComponent implements OnInit {
   }
 
 }
+
